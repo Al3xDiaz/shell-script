551c02af1b6c22b125043304e0444c500d95d266
Alex Leonel Diaz <47805592+Al3xDiaz@users.noreply.github.com>
2022-04-23 22:47:08 -0400

    Create comands.sh

---
+++ linux/comands.sh
@@ -0,0 +1,18 @@
+alias open=xdg-open
+gcommit() {
+    #do things with parameters like $1 such as
+    git branch
+    git add .
+    git status
+    sh ./test.sh && git commit -m "$1"
+}
+gpush(){
+    gcommit "$1" && git push
+}
+gstash(){
+    git add .
+    git stash $@
+}
+ipv4(){
+hostname -I | awk '{print $1}'
+}
